081d96cbaa08b39cb732263958b604da0afb5c60
shoma <h30i16@ed.cc.suzuka-ct.ac.jp>
2020-04-04 15:33:44 +0900

    font awesome関連

--- src/main.js
+++ src/main.js
@@ -2,10 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { faCoffee,faHandshake,faCode,faUserCircle,faHammer } from '@fortawesome/free-solid-svg-icons'
+import { faCoffee,faHandshake,faCode,faUserCircle,faHammer,faPen,faSchool,faSearch,faEnvelope } from '@fortawesome/free-solid-svg-icons'
+import { faTwitter,faGithub } from '@fortawesome/free-brands-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
-library.add(faCoffee,faHandshake,faCode,faUserCircle,faHammer)
+library.add(faCoffee,faHandshake,faCode,faUserCircle,faHammer,faPen,faSchool,faSearch,faTwitter,faGithub,faEnvelope)
 
 Vue.component('font-awesome-icon', FontAwesomeIcon)
 
